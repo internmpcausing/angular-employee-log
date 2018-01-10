945f802c4c934eba6c966feb4506abf3fc0ed196
internmpcausing <intern03.halcyondigital@gmail.com>
2018-01-10 19:23:33 +0800

    added socket.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { MapService } from './Services/googlemaps.service';
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as io from "socket.io-client";
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -8,12 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'app';
   lat: number = 37.4224764;
   lng: number = -122.0842499;
   address: string;
   zoom: number = 11;
+
+  socket = io('http://localhost:8080/',
+  {reconnect: true, transports : ['websocket']});
 
   constructor(private mapService:MapService){
     this.mapService.getFormattedAddress(this.lat, this.lng).subscribe(
@@ -23,6 +27,18 @@ export class AppComponent{
       }, err => {
 
       }
-    );
+    );    
   }
+   ngOnInit(){
+    this.socket.on('initRate', (data) => {
+      console.log(data);
+      this.socket.emit('signalz', 'received');
+    });
+    console.log('fgsdf');
+    this.socket.emit('get-init-data', 'fdsf');
+
+    this.socket.on('send-init-rate', data => {
+      console.log(data);
+    })
+   }
 }
