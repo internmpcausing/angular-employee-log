4329b240955ebded85ed96c33209198f9560a18c
internmpcausing <intern03.halcyondigital@gmail.com>
2018-01-12 20:19:04 +0800

    fetching notification from database and updating flag on seen event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,14 +23,15 @@ export class AppComponent implements OnInit{
   constructor(private mapService:MapService){
     this.mapService.getFormattedAddress(this.lat, this.lng).subscribe(
       data => {
-        //console.log((<any>data).results[0].formatted_address);
         this.address = (<any>data).results[0].formatted_address;
       }, err => {
 
       }
     );    
   }
-   ngOnInit(){
+
+  notifications:Object[] = new Array();
+  ngOnInit(){
     (<any>this.messages).push(
       {
         txt: 'fsdfd',
@@ -38,40 +39,79 @@ export class AppComponent implements OnInit{
         time: '8:30 1/11/18'
       }
     )
-    console.log(this.messages);
-    this.socket.on('initRate', (data) => {
+
+    this.socket.emit('cl-getinitnotif');
+    this.socket.on('sv-sendinitnotif', data => {
+      for(let item of data){
+        this.notifications.unshift(item);
+      }
       console.log(data);
-      this.socket.emit('signalz', 'received');
+    })
+
+    this.socket.on('sv-newnotification', data => {
+      this.notifications.unshift(data);
+      console.log(data);
     });
-    console.log('fgsdf');
 
-    this.socket.emit('get-init-notif');
+    this.socket.on('sv-newmessagetoadmin', data => {
+      (<any>this.messages).push(
+        {
+          txt: data.text,
+          isme: false,
+          time: '8:30 1/11/18'
+        }
+      )
+    });
 
-    this.socket.on('send-init-notif', data => {
+    this.socket.on('sv-servenotificationdetails', data => {
       console.log(data);
-    })
-
-    this.socket.on('sv-get-pic', data => {
-      console.log(data);
-      this.pic = data;
-    })
-
+      this.lat = data.map.lat;
+      this.lng = data.map.long
+      this.pic = data.pic;
+      this.mapService.getFormattedAddress(this.lat, this.lng).subscribe(
+        data => {
+          this.address = (<any>data).results[0].formatted_address;
+        }, err => {
+  
+        }
+      ); 
     
+      this.socket.emit('cl-join-room', 4324);
 
-   }
+      
+    });
 
-   messages:Object =  [] as Object;
+  }
+
+
+   messages:Object =  [] as Object; 
    @ViewChild('chatMessages') chatMessages: ElementRef
    sendNewMessage(txt: HTMLInputElement){
-    (<any>this.messages).push(
-      {
-        txt: txt.value,
-        isme: false,
-        time: '8:30 1/11/18'
-      }
-    )
+     let newmessage = {
+          txt: txt.value,
+          isme: true,
+          time: '8:30 1/11/18'
+        };
+    (<any>this.messages).push(newmessage);
+    this.socket.emit('cl-sendmessagetoemployee', newmessage);
     txt.value = '';
 
     this.chatMessages.nativeElement.scrollTop = this.chatMessages.nativeElement.scrollHeight;
+
+    // this.socket.emit('cl-timein',{
+    //   employeeid: 4324,
+    //   time: 'Fri, 12 Jan 2018 03:28:36 GMT',
+    //   map: { long: 121.0618299, lat: 14.5722277 },
+    //   bt: 100,
+    //   msg: 'Fhjwtj',
+    //   b64: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf6aaQJAo291z97N4QtmDBsG_WhZ9puznRWdlSLIKC5h6RtgqgeQ'
+    // });
+
+    
+   }
+
+   viewNotification(notification){
+     notification.isseen = true;
+     this.socket.emit('cl-getnotificationdetails',{notificationid: notification.notificationid})
    }
 }
